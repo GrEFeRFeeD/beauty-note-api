68a161118bbccab21bfeaf6566274c24a1c0bf66
GrEFeRFeeD <azofer77@gmail.com>
2023-05-16 12:58:20 +0300

    initial commit

---
+++ src/test/java/com/api/beautybook/BeautyBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.beautybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
